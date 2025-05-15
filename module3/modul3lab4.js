94e52c79532ce7300da9fbffcf8b2ecee5e2af17
Tony Knudson <tonyknudson91@gmail.com>
2025-05-15 10:19:37 -0500

    added new exercise and added to notes

---
+++ module3/modul3lab4.js
@@ -0,0 +1,51 @@
+// Task 1: Code a Person class
+class Person{
+    constructor(name = 'Tom', age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+    }
+
+    doSomethingFun() {
+        if (this.energy > 0) {
+            this.energy -= 10;
+            console.log('Energy is decreasing, currently at:', this.energy);
+        } else if (this.energy <= 0 ) {
+            this.sleep;
+        }
+        
+    }
+}
+
+// Task 2: Code a Worker class
+class Worker extends Person {
+    constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+        super(name, age, energy)
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+
+    goToWork() {
+        this.xp += 10;
+        console.log("xp increasing, currently at: ", this.xp);
+    }
+}
+console.log("Intern:", intern());
+console.log("Manager:", manager());
+
+
+function intern() {
+    var intern = new Worker("Bob", 21, 110, 0, 10);
+    intern.goToWork();
+    return intern;
+
+}
+
+function manager() {
+    var manager = new Worker("Alice", 30, 120, 100, 30);
+    manager.doSomethingFun();
+    return manager;
+}
